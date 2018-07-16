6cd360e0a24259d19c224755f6d23d4d89941117
am-gooru <ashish@gooru.org>
2018-07-16 12:00:20 +0530

    Teacher suggestions for class courses flow completion. Fix up the end to end flow for teacher suggestions applicability for classic courses

--- src/main/java/org/gooru/navigatemap/processor/next/pathfinder/StraightPathFinderService.java
+++ src/main/java/org/gooru/navigatemap/processor/next/pathfinder/StraightPathFinderService.java
@@ -21,6 +21,7 @@ import org.slf4j.LoggerFactory;
 class StraightPathFinderService implements PathFinder {
     private final DBI dbi;
     private static final Logger LOGGER = LoggerFactory.getLogger(StraightPathFinderService.class);
+    private PathFinderContext context;
 
     StraightPathFinderService(DBI dbi) {
         this.dbi = dbi;
@@ -28,13 +29,32 @@ class StraightPathFinderService implements PathFinder {
 
     @Override
     public PathFinderResult findPath(PathFinderContext context) {
+        this.context = context;
         if (context.getContentAddress().isOnSystemPath() || context.getContentAddress().isOnRoute0()) {
             throw new HttpResponseWrapperException(HttpConstants.HttpStatus.BAD_REQUEST,
                 "Route0 or system path navigation is disabled");
         }
+        if (context.getContentAddress().isOnTeacherPath()) {
+            return loadNextItemFromTeacherpath();
+        }
         return new PathFinderResult(
             ContentFinderFactory.buildTeacherPathAwareMainPathContentFinder(dbi, ContentFinderCriteria.CRITERIA_VISIBLE)
                 .findContent(context));
+    }
+
+    private PathFinderResult loadNextItemFromTeacherpath() {
+        ContentAddress teacherPathContentAddress =
+            ContentFinderFactory.buildTeacherPathContentFinder(dbi).findContent(context);
+        if (teacherPathContentAddress != null) {
+            return new PathFinderResult(teacherPathContentAddress);
+        } else {
+            return loadCurrentItemFromMainpath();
+        }
+    }
+
+    private PathFinderResult loadCurrentItemFromMainpath() {
+        return new PathFinderResult(
+            ContentFinderFactory.buildAlternatePathUnawareSpecifiedPathContentFinder(dbi).findContent(context));
     }
 
 }
