97c3e5fef6532b1d7825924494cf902fe9e5f00d
am-gooru <ashish@gooru.org>
2018-07-27 17:08:59 +0530

    Empty suggestions scenario. When there are suggestions id from signature items table, and during suggestion card creation, they vanish (the actual items in collection table is deleted or does not exist), then put detailed logging in log files. In addition, apply suggestion limit after card is created so that if any one item is present in db, we present that to user instead of selecting first id and looking up in table which may not exist. Provide an optimization path also, so that when previous state was done, then instead of doing all processing and getting state done back, just short circuit the processing and return done.

--- src/main/java/org/gooru/navigatemap/infra/data/RequestContext.java
+++ src/main/java/org/gooru/navigatemap/infra/data/RequestContext.java
@@ -98,6 +98,10 @@ public final class RequestContext {
         return (state == State.Start && currentItemId != null);
     }
 
+    public boolean isCourseCompleted() {
+        return state == State.Done;
+    }
+
     public boolean onMainPath() {
         return (getPathId() == null || getPathId() == 0) && getPathType() == null;
     }
